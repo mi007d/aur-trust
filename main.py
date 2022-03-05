da242b74a7ce65703bf966dffda9e62c355c73b6
mi007d <github.ni9qs@simplelogin.co>
2022-03-05 20:15:52 -0300

    Added emojis 🔥

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+import emoji
+
 import urllib3
 import json
 
@@ -5,15 +8,16 @@ package_name = input("Package name: ")
 
 # Starting request
 http = urllib3.PoolManager()
-r = http.request('GET', f'https://aur.archlinux.org/rpc/?v=5&type=search&arg={package_name}')
+response = http.request('GET', f'https://aur.archlinux.org/rpc/?v=5&type=search&arg={package_name}')
 
 # Getting JSON result list
-results = json.loads(r.data.decode('utf-8'))['results']
+results = json.loads(response.data.decode('utf-8'))['results']
 
 # "Seeing" all list and checking if the package name is equals to the value passed on package_name (the variable)
 for result in results:
     if result['Name'] == package_name:
-        print('Name:', result['Name'])
-        print('URL:', result['URL'])
-        print('Popularity:', result['Popularity'])
-        print('Votes:', result['NumVotes'])
+        os.system("clear")
+        print(emoji.emojize(':package: ', use_aliases=True) + f'Name:', result['Name'])
+        print(emoji.emojize(':dart: ', use_aliases=True) + 'URL:', result['URL'])
+        print(emoji.emojize(':fire: ', use_aliases=True) + f'Popularity:', result['Popularity'])
+        print(emoji.emojize(':top: ', use_aliases=True) + 'Votes:', result['NumVotes'])
