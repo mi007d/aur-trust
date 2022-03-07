7ffef1b7f5b85dc4b3c6afce070d22907ce69080
mi007d <github.ni9qs@simplelogin.co>
2022-03-06 21:48:36 -0300

    Better(?) string format

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ results = json.loads(response.data.decode('utf-8'))['results']
 for result in results:
     if result['Name'] == package_name:
         os.system("clear")
-        print(emoji.emojize(':package: ', use_aliases=True) + f'Name:', result['Name'])
-        print(emoji.emojize(':dart: ', use_aliases=True) + 'URL:', result['URL'])
-        print(emoji.emojize(':fire: ', use_aliases=True) + f'Popularity:', result['Popularity'])
-        print(emoji.emojize(':top: ', use_aliases=True) + 'Votes:', result['NumVotes'])
+        print(f'{emoji.emojize(":package:", use_aliases=True)} Name: {result["Name"]}')
+        print(f'{emoji.emojize(":dart:", use_aliases=True)} URL: {result["URL"]}')
+        print(f'{emoji.emojize(":fire:", use_aliases=True)} Popularity: {result["Popularity"]:.0f}')
+        print(f'{emoji.emojize(":top:", use_aliases=True)} Votes: {result["NumVotes"]:.0f}')
