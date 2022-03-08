1f1aa964b5832a12f6878fddfbf702c4ba0f8042
mi007d <github.ni9qs@simplelogin.co>
2022-03-08 20:26:20 -0300

    Fixed emoji implementation

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ results = json.loads(response.data.decode('utf-8'))['results']
 for result in results:
     if result['Name'] == package_name:
         os.system("clear")
-        print(f'{emoji.emojize(":package:", use_aliases=True)} Name: {result["Name"]}')
-        print(f'{emoji.emojize(":dart:", use_aliases=True)} URL: {result["URL"]}')
-        print(f'{emoji.emojize(":fire:", use_aliases=True)} Popularity: {result["Popularity"]:.0f}')
-        print(f'{emoji.emojize(":top:", use_aliases=True)} Votes: {result["NumVotes"]:.0f}')
+        print(f'{emoji.emojize(":package:")} Name: {result["Name"]}')
+        print(f'{emoji.emojize(":bullseye:")} URL: {result["URL"]}')
+        print(f'{emoji.emojize(":fire:")} Popularity: {result["Popularity"]:.0f}')
+        print(f'{emoji.emojize(":up_arrow:")} Votes: {result["NumVotes"]:.0f}')
